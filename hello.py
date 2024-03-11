4636851af03fca62a6d5e8ecb3219b9153d7b324
Benny Iko <benny.iko.it@gmail.com>
2024-03-11 02:24:47 -0400

    On branch main: adds comments hello.py 0.0.2 Changes to be committed: 	modified:   hello.py 	adds comments to hello.py 0.0.2

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+"""When you run a Python module like
+
+python hello.py
+
+the code in the module will be executed, just as if you imported it, but with the __name__ set to "__main__". That means that by adding this code at the end of your module:you can make the file usable as a script as well as an importable module, because the code that parses the command line only runs if the module is executed as the “main” file"""
 
 if __name__ == "__main__":
     print("hello world")
