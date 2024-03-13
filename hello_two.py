4cf5c0757f87b4df69b9bab24eb818d820f0ea46
Benny Iko <benny.iko.it@gmail.com>
2024-03-13 19:51:06 -0400

    On branch main: Adds call to hello script 0.0.3 Changes to be committed: 	modified:   hello_two.py 	adds call to hello script from hello_two script. 	KNOWN BUG: can not see standard output from hello call.
    	Was able to produce ModuleNotFoundError:
    
    	$ python hello_two.py 5
    	hello world greeting #1
    	hello world greeting #2
    	hello world greeting #3
    	hello world greeting #4
    	Traceback (most recent call last):
      File "C:\Users\benny\Documents\Projects\simple_python_script_demo\hello_two.py", line 19, in <module>
        importlib.import_module("hello.py")
      File "C:\Program Files\Python311\Lib\importlib\__init__.py", line 126, in import_module
        return _bootstrap._gcd_import(name[level:], package, level)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "<frozen importlib._bootstrap>", line 1206, in _gcd_import
      File "<frozen importlib._bootstrap>", line 1178, in _find_and_load
      File "<frozen importlib._bootstrap>", line 1137, in _find_and_load_unlocked
    	ModuleNotFoundError: No module named 'hello.py'; 'hello' is not a package

--- hello_two.py
+++ hello_two.py
@@ -11,6 +11,9 @@ hello world greeting #4
 the code in the module will be executed, just as if you imported it, but with the __name__ set to "__main__". That means that by adding this code at the end of your module:you can make the file usable as a script as well as an importable module, because the code that parses the command line only runs if the module is executed as the “main” file"""
 
 if __name__ == "__main__":
+    import hello
     import sys
+    import importlib
     for num in range(1, int(sys.argv[1])):
         print("hello world greeting #" + str(num))
+    importlib.import_module("hello.py")
