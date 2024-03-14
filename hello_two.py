007d07386afeea38f38803cf16b2e14371d1416d
Benny Iko <benny.iko.it@gmail.com>
2024-03-14 01:08:31 -0400

    On branch main: adds comments explaining script hello_two.py 0.0.4 Changes to be committed: 	modified:   hello_two.py 	adds comments explaining issues with script. 	if __name__ == __main__ is for program entry point only

--- hello_two.py
+++ hello_two.py
@@ -17,3 +17,92 @@ if __name__ == "__main__":
     for num in range(1, int(sys.argv[1])):
         print("hello world greeting #" + str(num))
     importlib.import_module("hello.py")
+    
+""" Why this does not work:
+
+__main__ — Top-level code environment
+https://docs.python.org/3/library/__main__.html
+
+SHORT ANSWER:
+
+
+if __name__ == '__main__' code block comes in handy. 
+Code within this block won’t run unless the module is executed in the top-level environment.
+
+Putting as few statements as possible in the block below if __name__ == '__main__' can improve code clarity and correctness. 
+Most often, a function named main encapsulates the program’s primary behavior:
+
+CODE EXAMPLE:
+
+# echo.py
+
+import shlex
+import sys
+
+def echo(phrase: str) -> None:
+   A dummy wrapper around print
+   # for demonstration purposes, you can imagine that there is some
+   # valuable and reusable logic inside this function
+   print(phrase)
+
+def main() -> int:
+    Echo the input arguments to standard output
+    phrase = shlex.join(sys.argv)
+    echo(phrase)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())  # next section explains the use of sys.exit
+
+
+LONG ANSWER:
+
+
+
+In Python, the special name __main__ is used for two important constructs:
+
+1. the name of the top-level environment of the program,
+which can be checked using the __name__ == '__main__' expression; and
+
+2. the __main__.py file in Python packages.
+
+What is the “top-level code environment”?
+__main__ is the name of the environment where top-level code is run.
+“Top-level code” is the first user-specified Python module that starts running. 
+It’s “top-level” because it imports all other modules that the program needs. 
+Sometimes “top-level code” is called an entry point to the application.
+
+The top-level code environment can be:
+
+the scope of an interactive prompt:
+
+>>>
+__name__
+'__main__'
+the Python module passed to the Python interpreter as a file argument:
+
+python helloworld.py
+Hello, world!
+the Python module or package passed to the Python interpreter with the -m argument:
+
+python -m tarfile
+usage: tarfile.py [-h] [-v] (...)
+Python code read by the Python interpreter from standard input:
+
+echo "import this" | python
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+...
+Python code passed to the Python interpreter with the -c argument:
+
+python -c "import this"
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+...
+In each of these situations, the top-level module’s __name__ is set to '__main__'.
+
+"""
